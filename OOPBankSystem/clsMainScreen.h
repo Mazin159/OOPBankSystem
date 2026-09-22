b0ccfdbf52b1a6f733562a51ad37df555edf934b
MazinMoh <mazinmoh@gmail.com>
2026-09-22 20:09:11 +0300

    Mange Users screen

--- OOPBankSystem/clsMainScreen.h
+++ OOPBankSystem/clsMainScreen.h
@@ -9,6 +9,7 @@
 #include "clsUpdateClientScreen.h"
 #include "clsFindClientScreen.h"
 #include "clsTransactionsScreen.h"
+#include "clsManageUsersScreen.h"
 
 
 class clsMainScreen : protected clsScreen
@@ -136,8 +137,7 @@ class clsMainScreen : protected clsScreen
 
 	static void _ShowManageUsersMenue()
 	{
-		cout << "\nUsers Menue Will be here...\n";
-
+		clsManageUsersScreen::ShowManageUsersMenue();
 	}
 
 	static void _ShowEndScreen()
